f034f3efddb7d8722cdb579af216c8b7693b49c1
ke-to <kuronokei0606@gmail.com>
2016-02-20 01:27:53 +0900

    Added files via upload
    export TXT file.

--- mecab-sumple.py
+++ mecab-sumple.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*- 
 
 from natto import MeCab
+import re
 
 nm = MeCab()
 #print(nm)
 
 content = ""
-fileName = '/Users/kojimakeito/Desktop/result2.txt'
+fileName = raw_input("Output File name write now! > ")
+fileName2 = raw_input("Input file name write now! > ")
+
 result = open(fileName, 'w')
 result.write("")
 
 with MeCab('-F%f[1]') as nm:
-	f = open('/Users/kojimakeito/Desktop/result.txt', 'r')
+	f = open(fileName2, 'r')
 	for line in f:
 		text = line
 		for n in nm.parse(text, as_nodes=True):
-			content += ',%s' % n.surface
-			result.writelines(n.surface)
-			#print n
+			if re.compile('形容詞|感動詞|固有名詞').search(n.feature):
+				content += ',%s' % n.surface
+				result.writelines(',%s' % n.surface)
+				print n.surface
 
 
 f.close()
